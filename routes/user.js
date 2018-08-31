51cbf1eaf75325df81d7c3ed2cfc082363a9a7de
Raman Prabhakar <ramanprabhakar007@gmail.com>
2018-08-31 14:49:16 +0530

    delete unnecessary code parts

--- routes/user.js
+++ routes/user.js
@@ -23,25 +23,6 @@ module.exports = function(app){
 
 	app.get('/profile', function(req, res, next){
 		res.render('accounts/profile', {message : req.flash('loginMessage')});
-	})
-
-
-
-
-	/*app.get('/auth/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
-*/
+	});
 
 }
